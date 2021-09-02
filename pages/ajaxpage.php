605c5c417ace88f92ba02c30dee4c9a5e8371d98
Ryan Antcliff <antcliffr12@gmail.com>
2021-09-02 12:29:56 -0400

    changed mime

--- pages/ajaxpage.php
+++ pages/ajaxpage.php
@@ -1,7 +1,7 @@
 
 
 <?php
-$mimes = array('text/csv');
+$mimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain');
 $message = "";
     if (in_array($_FILES['file']['type'], $mimes)) {
         $info = pathinfo($_FILES['file']['name']);
