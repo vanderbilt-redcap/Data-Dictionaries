f7bd6e419fef0b209fd6975f1436ffddd0001d60
knil-maloon <linkedfrends@gmail.com>
2022-07-27 11:58:52 -0500

    Text added

--- pages/Data.php
+++ pages/Data.php
@@ -5,6 +5,11 @@ require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
 // print_array($module->getUrl('test.php'));
 $tesst = $module->getUrl("images/1484.gif");
 ?>
+<h6 class="container">
+    Drag and drop a REDCap Data Dictionary CSV. It will be compared with the data dictionary of the current project. Added, deleted, or revised variables will be listed below.
+</h6>
+<br />
+<br />
 <div class="container-fluid p-y-1">
     <div class="row m-b-1">
         <div class="col-sm-6 offset-sm-3">
